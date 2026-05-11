c8fd62c84fb54002fb25c3808476d74b7a0785c7
sko4ec <psw080401@gmail.com>
2026-05-11 12:40:00 +0900

    changed logic + added pyo

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const htag = document.getElementById("htag")
 const c = 299792.458;
 
-function calc_origin_freq() {
-    const v = parseFloat(document.getElementById('velocity').value);
-    const f_obs = parseFloat(document.getElementById('obs_frequency').value);
+function calc_origin_freq(vel, obs_f) {
 
-    if (v >= c) return alert("속도는 광속보다 작아야 합니다.");
+    vel = c * vel/100
 
-    const f_s = f_obs / Math.sqrt((c + v) / (c - v));
-
-    console.log("Original Frequency:", f_s.toFixed(6), "THz");
-    htag.innerHTML = f_s.toFixed(6) + "THz"
-    return f_s;
+    return obs_f/(Math.sqrt( (c+vel)/(c-vel) ))
 }
 
-function calc_speed() {
-    const f_s = parseFloat(document.getElementById('s_frequency').value);
-    const f_obs = parseFloat(document.getElementById('obs_frequency1').value);
-
-    const fs2 = Math.pow(f_s, 2);
-    const fobs2 = Math.pow(f_obs, 2);
-
-    const v = c * (fobs2 - fs2) / (fobs2 + fs2);
-
-    console.log("Velocity:", v, "km/s");
-    htag.innerHTML = v.toFixed(6) + "km/s"
-
-    return v;
+function calc_speed(f, obs_f) {
+    let K = Math.pow(obs_f/f, 2)
+    console.log(K)
+    return c*(K-1)/(K+1)
 }
 
 
-function rel_dopp_blue(f, v) {
-    const fs = Number(f);
-    const vel = Number(v);
-    htag.innerHTML = fs * Math.sqrt((c + vel) / (c - vel)).toFixed(6) + "THz"
-    return fs * Math.sqrt((c + vel) / (c - vel));
+function calc_shifted_freq(f, v) {
+    f *= 1e12
+    v = c * v/100
+    const result = Math.sqrt((c+v)/(c-v)) * f*1e-12
+    console.log(result)
+    return result
 }
